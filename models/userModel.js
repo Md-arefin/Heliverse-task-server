9494d67605fdcf27fb9aa8cf526f5ddf69502f06
Md-arefin <ansariarefin617@gmail.com>
2023-11-18 21:27:35 +0600

    mongoose init

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    _id: String,
+    first_name: String,
+    last_name: String,
     email: String,
-    password: String
+    gender: String,
+    avatar: String,
+    domain: String,
+    available: String,
 });
 
 const UserModel = mongoose.model('users', userSchema);
